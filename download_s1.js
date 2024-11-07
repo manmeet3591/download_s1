7f9735f81588049e1ded413f8f46e25f815354c1
Manmeet Singh <manmeet20singh11@gmail.com>
2024-11-07 17:17:20 -0600

    Update download_s1.js

--- download_s1.js
+++ download_s1.js
@@ -1,6 +1,6 @@
 // Define the area of interest (AOI) as Austin, Texas with a rectangular buffer
 var austinCoords = ee.Geometry.Point([-98.46, 30.18]);
-var bufferSize = 60000; // 10 km
+var bufferSize = 80000; // 10 km
 
 // Create a rectangular buffer around the point
 var aoi = austinCoords.buffer(bufferSize).bounds();
